029e06638a94eadc42a81855e32411980195a518
Aaron <32709100+Acekiller433@users.noreply.github.com>
2017-12-02 15:32:46 -0500

    Update FloorDesign.h

--- FloorDesign.h
+++ FloorDesign.h
@@ -1,1 +1,31 @@
 
+/*
+ * FloorDesign.h
+ *
+ *  Created on: Nov 30, 2017
+ *      Author: Owner
+ */
+
+#include <cstdlib>
+#include <iostream>
+//#include "CombatSystem.cpp"
+//#include "CharacterCreation.cpp"
+#include "MonsterGeneration.cpp"
+
+class Floor {
+private:
+	int level;
+	//Character hero;
+
+public:
+	Floor(int level);
+	int getLevel();		//added getLevel() for monsterGeneration
+	bool run();
+	bool runShop();
+	void runStairs();
+	bool runMonster();
+	void runLoot();
+	void runNothing();
+	bool runBoss();
+
+};
