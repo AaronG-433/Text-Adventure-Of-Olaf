223217c87e00ad3e4c07aa47251f30cfb5268b9c
Aaron <32709100+Acekiller433@users.noreply.github.com>
2017-12-02 23:15:39 -0500

    Update CombatSystem.h

--- CombatSystem.h
+++ CombatSystem.h
@@ -1,1 +1,20 @@
 
+/*
+ * CombatSystem.h
+ *
+ *  Created on: Nov 30, 2017
+ *      Author: Owner
+ */
+
+
+using namespace std;
+#include <iostream>
+#include <string>
+#include <sstream>		//imports the stringstream function
+#include <cctype>		//imports the isspace() function
+
+class CombatSystem {
+public:
+	CombatSystem();
+	virtual ~CombatSystem();
+};
