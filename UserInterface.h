5656eba73b4b01c9d0d2ee87e1d50d192204e33a
jaspera11 <33080050+jaspera11@users.noreply.github.com>
2017-12-03 18:22:05 -0500

    Update UserInterface.h

--- UserInterface.h
+++ UserInterface.h
@@ -5,15 +5,22 @@ using namespace std;
 
 class UserInterface {
 private:
-    int selectionNum;
+   // int selectionNum;
     int storyPart;
-    
+
 public:
-    
+
     //Creates the story and accesses different parts
     void createStory ();
     int getStoryPart ();
     void setStoryPart (int part);
+    void finishLvl(int part);
+    void beginEnd(int x);
+    void elipsis();
+    void opener(const string& message);
+    void lag(int n);
+
+
 
 };
 
