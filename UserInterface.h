4cebd66add9a0fe513570fac2d105f4027f303c6
mleonard0197 <33047210+mleonard0197@users.noreply.github.com>
2017-12-03 10:53:35 -0500

    Update UserInterface.h

--- UserInterface.h
+++ UserInterface.h
@@ -1,1 +1,21 @@
+#ifndef UserInterface_h
+#define UserInterface_h
+using namespace std;
+#include <string.h>
 
+class UserInterface {
+private:
+    int selectionNum;
+    int storyPart;
+    
+public:
+    
+    //Creates the story and accesses different parts
+    void createStory ();
+    int getStoryPart ();
+    void setStoryPart (int part);
+
+};
+
+
+#endif
