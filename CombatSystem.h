496970e4a7badb3225916c0b6fddd4a8cbffd8e6
jaspera11 <33080050+jaspera11@users.noreply.github.com>
2017-12-03 20:45:48 -0500

    Update CombatSystem.h

--- CombatSystem.h
+++ CombatSystem.h
@@ -12,6 +12,11 @@ using namespace std;
 #include <string>
 #include <sstream>		//imports the stringstream function
 #include <cctype>		//imports the isspace() function
+#include "CharacterCreation.h"
+#include "CharacterCreation.cpp"
+#include "MonsterGeneration.h"
+#include "MonsterGeneration.cpp"
+
 
 class CombatSystem {
 public:
