0f417fd9879b0621381dd8561786a076c126e4f3
jaspera11 <33080050+jaspera11@users.noreply.github.com>
2017-12-03 20:46:47 -0500

    Update CharacterCreation.cpp

--- CharacterCreation.cpp
+++ CharacterCreation.cpp
@@ -2,17 +2,7 @@
 *Created By: Dylan Alvarez
 *Contains:The methods that go with the classes defined by the Character_Creation_H
 */
-#include "FloorDesign.h"
-#include "CombatSystem.h"
-#include "MonsterGeneration.h"
-#include "CharacterCreation.h"
-#include "UserInterface.h"
-#include "FloorDesign.cpp"
-#include "CombatSystem.cpp"
-#include "MonsterGeneration.cpp"
-#include "CharacterCreation.cpp"
-#include <iostream>
-#include <string>
+
 #include "CharacterCreation.h"
 
 //Defining the methods inherited from the abstract Player class
